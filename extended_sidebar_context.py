7bafd1e91d999b8338c5912633ef76437928f24d
Gwen Reynolds <gwen.reynolds.94@gmail.com>
2022-07-17 11:29:41 -0400

    22 07 17 [11:29]

--- extended_sidebar_context.py
+++ extended_sidebar_context.py
@@ -1,5 +1,18 @@
 import sublime
 import sublime_plugin
+from pathlib import Path
+
+
+DEFAULT_PROJECT_FILE_TEXT = """{
+	"folders":
+	[
+		{
+			"path": "."
+		}
+	]
+}"""
+
+
 
 class InstanceCache:
 	def __init__(self, logCom = False, logInput = False, nameSett = "Preferences.sublime-settings"):
@@ -33,14 +46,68 @@ class InstanceCache:
 			self.work_file = self.justFound.workspace_file_name() or None
 			
 
-class FilesAndFolders:
-	def __init__(self, window):
-		self.window = window or sublime.active_window()
-		self.proj_file = window.project_file_name()
-		self.work_file = window.workspace_file_name()
 
+class FilesAndFolders:
+	def __init__(self, window = None):
+		self.window = window if window else sublime.active_window()
+		self.proj_file = self.window.project_file_name()
+		self.work_file = self.window.workspace_file_name()
+		self.parent = None
+		self.everything = None
+		self.all_file_paths = None
+		self.project_added = None
+		
+	def updateChanges(self, window = None):
+		self.window = window if window else sublime.active_window()
+		self.proj_file = self.window.project_file_name()
+		self.work_file = self.window.workspace_file_name()
+		
+	def getParentFolder(self, should_return = False):
+		if not self.everything:
+			self.getEverything()
+		self.parent = sublime.expand_variables("${file_path}", self.everything)
+		if should_return:
+			return self.parent
+		
+	def getEverything(self, should_return = False, window = None):
+		if not self.window and not window:
+			self.window = sublime.active_window()
+		self.everything = self.window.extract_variables()
+		if should_return:
+			return self.everything
+			
+	def printEverything(self, only_keys = False, only_values = False):
+		if not self.everything:
+			self.getEverything()
+		print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
+		for k in self.everything:
+			print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
+			print(k)
+			print(self.everything[k])
+		print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
+		
+	def getAllFilePaths(self, should_return = False, window = None):
+		if not self.everything or not self.window:
+			self.getEverything()
+		self.all_file_paths = sublime.expand_variables("${file_path}", self.everything)
+		if should_return:
+			return self.all_file_paths
+
+	def addParentFolder(self, file_name = False):
+		if not self.parent or not self.window:
+			self.getParentFolder()
+			self.window = sublime.active_window()
+		if project_added:
+			sublime.status_message("Cannot add folder if project settings already intialized")
+			return None
+		else:
+			self.open_folders = self.window.folders()
+		
+		
+		
 
 # window.extract_variables() -> possible combinations of...
+# 
 # [Dict]
 # "packages"
 # "platform"
@@ -55,16 +122,26 @@ class FilesAndFolders:
 # "project_name"
 # "project_base_name"
 # "project_extension"
+# 
 # fullList = sublime.active_window().extract_variables()
 # lookUp = sublime.expand_variables("${file_path}", fullList) -> focused file's path
 # window.find_open_file(file_name) -> view
 # window.folders() -> 
 		
-		
 
 class ExtendedSidebarContextCommand(sublime_plugin.WindowCommand):
 	def run(self, **args):
 		return
+		
+
+class AddFolderFromOpenFileCommand(sublime_plugin.WindowCommand):
+	def run(self, group, index):
+		f_and_f = FilesAndFolders()
+		p_dir = f_and_f.getParentFolder(True)
+		a_f_f = f_and_f.getEverything(True)
+		f_and_f.printEverything()
+
+
 		
 class SyncSettingsCache(sublime_plugin.EventListener):
 	def on_init(self, views):
@@ -77,7 +154,7 @@ class SyncSettingsCache(sublime_plugin.EventListener):
 		# print(self.cache.work_file)
 		# vList = sublime.active_window().extract_variables()
 		# xList = sublime.expand_variables("${file_path}", vList)
-		print(sublime.active_window().folders())
+		# print(sublime.active_window().folders())
 		
 	# def on_window_command(self, window, command_name, args):
 	# 	return
